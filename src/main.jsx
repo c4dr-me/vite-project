a2b5a64edebfeae7aed9e7bc136e1bf09194f04e
c4dr-me <142721314+c4dr-me@users.noreply.github.com>
2024-05-20 11:46:31 +0530

    added dark mode

---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+// src/main.jsx
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+
+// Create a root
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Render the app
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
